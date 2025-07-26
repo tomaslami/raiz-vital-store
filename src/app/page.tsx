721c839c1b75b0dc51a66833f22a8e655f9b0d9c
tomaslami <tomaslamiguralnik@gmail.com>
2025-07-26 17:50:33 -0300

    Remove unused Contact component import from Home page to streamline code and improve maintainability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import OurProcess from "@/components/our-process/our-process"
 import FeaturedProducts from "@/components/featured-products/featured-products"
 import AboutUs from "@/components/about-us/about-us"
 import WhyChooseUs from "@/components/why-choose-us/why-choose-us"
-import Contact from "@/components/contact/contact"
 
 export default function Home() {
   return (
